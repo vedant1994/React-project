41626b04c30e661b55521d0355084970921a483e
Vedant Rathavi <vedantrathavi2006@gmail.com>
2025-05-12 19:14:16 +0530

    Updated

--- ems/src/main.jsx
+++ ems/src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/AuthContext.jsx'
-import TaskContext from './context/TaskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <AuthContext>
-      <TaskContext>
+    
         <App />
-      </TaskContext>
-    </AuthContext>
+      
 )
